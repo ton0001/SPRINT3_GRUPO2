c60b63e173837fc76078a6952f3bea938a0c3f7a
victoriacampiotti <victoriacampiotti@cencosud.com>
2022-10-03 16:28:33 -0300

    Agregando paquetes de testeo

--- database/config/config.js
+++ database/config/config.js
@@ -11,11 +11,13 @@ module.exports = {
     "logging": false
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "mysql",
+    "logging": false
   },
   "production": {
     "username": "root",
