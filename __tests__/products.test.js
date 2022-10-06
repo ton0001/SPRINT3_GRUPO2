46e9da8ba1fc0ace4fe0f2dc79a69d202a510684
luuroldan <arq.rodriguezroldan@gmail.com>
2022-10-06 00:39:45 -0300

    post de product - get de user y product completo

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -3,80 +3,231 @@ const { app, server } = require("../server");
 const { generateJWT } = require("../helpers/generateJWT");
 const initModels = require("../database/models/init-models");
 const { sequelize } = require("../database/models");
+const models = initModels(sequelize);
 
 afterEach(() => {
   server.close();
 });
 
+//test de ruta POST /products
 describe("POST /api/v3/products", () => {
-  //testear si puede crear un producto
-  test("Debe devolver un código de esatdo 200 y crear un producto nuevo", async () => {
-    // id para testear
-    // let idProduct = 108;
+  test("Debe devolver un estado 200 si el producto fue creado correctamente", async () => {
+    //datos para crear un producto
+    const data = {
+      title: "Producto de prueba",
+      price: 100,
+      description: "Producto de prueba",
+      category: 1,
+    };
+    //creacion del token
+    const god_user = {
+      id: 1,
+      username: "siacobo0",
+    };
+    const token = await generateJWT(god_user);
 
-    const data = [
-      {
-        title: "test",
-        price: 100,
-        description: "test",
-        category_id: 1,
-        mostwanted: 1,
-        stock: 10,
-      },
-    ];
-
-    // envio de los datos
+    //envio de los datos
+    const originalDB = await models.products.findAll();
+    console.log("originalDB:", originalDB.length);
     const { statusCode, body } = await request(app)
-      .post("/api/v3/products")
+      .post(`/api/v3/products`)
+      .auth(token, { type: "bearer" })
       .send(data);
 
-    // comprobacion del status
+    const newDB = await models.products.findAll();
+    console.log("newDB:", newDB.length);
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(200);
+    expect(originalDB.length + 1).toBe(newDB.length);
+
+    // comprobar respuesta
+    expect(body).toEqual;
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: expect.any(Number),
+        title: data.title,
+        price: data.price,
+        description: data.description,
+        category_id: data.category,
+      }),
+    ]);
+  });
+  //Debe devolver un estado 400 si faltan datos para crear el producto
+  test("Debe devolver un estado 400 si faltan datos para crear el producto", async () => {
+    //datos para crear un producto
+    const data = {
+      title: "Producto de prueba",
+      price: 100,
+      description: "Producto de prueba",
+    };
+    //creacion del token
+    const god_user = {
+      id: 1,
+      username: "siacobo0",
+    };
+    const token = await generateJWT(god_user);
+
+    //envio de los datos
+    const originalDB = await models.products.findAll();
+    console.log("originalDB:", originalDB.length);
+    const { statusCode, body } = await request(app)
+      .post(`/api/v3/products`)
+      .auth(token, { type: "bearer" })
+      .send(data);
+
+    const newDB = await models.products.findAll();
+    console.log("newDB:", newDB.length);
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(400);
+    expect(originalDB.length).toBe(newDB.length);
+
+    // comprobar respuesta
+    expect(body).toEqual;
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: expect.any(Number),
+        title: data.title,
+        price: data.price,
+        description: data.description,
+        category_id: data.category,
+      }),
+    ]);
+  });
+
+  //Debe devolver un estado 401 si no se envia el token
+  test("Debe devolver un estado 401 si no se envia el token", async () => {
+    //datos para crear un producto
+    const data = {
+      title: "Producto de prueba",
+      price: 100,
+      description: "Producto de prueba",
+      category: 1,
+    };
+
+    //envio de los datos
+    const originalDB = await models.products.findAll();
+    console.log("originalDB:", originalDB.length);
+    const { statusCode, body } = await request(app)
+      .post(`/api/v3/products`)
+      .send(data);
+
+    const newDB = await models.products.findAll();
+    console.log("newDB:", newDB.length);
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(401);
+    expect(originalDB.length).toBe(newDB.length);
+
+    // comprobar respuesta
+    expect(body).toEqual;
+    expect.arrayContaining([
+      expect.objectContaining({
+        id: expect.any(Number),
+        title: data.title,
+        price: data.price,
+        description: data.description,
+        category_id: data.category,
+      }),
+    ]);
+  });
+});
+
+//test de ruta get product/:id
+describe("GET /api/v3/products/:id", () => {
+  test("Producto encontrado correctamente", async () => {
+    // datos para obtener un producto
+    const data = {
+      id: 10,
+    };
+
+    // creacion del token
+    const god_user = {
+      id: 1,
+      username: "siacobo0",
+    };
+    const token = await generateJWT(god_user);
+
+    const product = await models.products.findByPk(data.id);
+
+    //envio de los datos
+    const { statusCode, body } = await request(app)
+      .get(`/api/v3/products/${data.id}`)
+      .auth(token, { type: "bearer" })
+      .send(data);
+
+    // comprobar codigo de status
     expect(statusCode).toBe(200);
 
-    // comprobacion de la respuesta
+    // comprobar respuesta
     expect(body).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          id: expect.any(Number),
-          title: expect.any(String),
-          price: expect.any(String),
-          description: expect.any(String),
-          category_id: expect.any(Number),
-          stock: expect.any(Number),
-          mostwanted: expect.any(Boolean),
-        }),
-      ])
+      expect.objectContaining({
+        id: data.id,
+        title: product.dataValues.title,
+        price: product.dataValues.price,
+        description: product.dataValues.description,
+        category_id: product.dataValues.category_id,
+        mostwanted: product.dataValues.mostwanted,
+        stock: product.dataValues.stock,
+      })
     );
   });
 
-  //testear que sea un usuario autirizado con rol GOD
-  //   test("Debe devolver un código de esatdo 401 si no esta loggeado como GOD", async () => {
-  //     const token = await verifyJWT({ role: "GOD" });
-  //     const { statusCode, body } = await (
-  //       await request(app).post("/api/v3/products")
-  //     ).auth(token, { type: "bearer" });
-  //     expect(statusCode).toBe(401);
-  //     expect(body).toEqual(
-  //       expect.arrayContaining([
-  //         expect.objectContaining({
-  //           ok: false,
-  //           msg: "No estas autorizado para realizar esta acción",
-  //         }),
-  //       ])
-  //     );
-  //   });
+  //Test debe devolver un estado 401 si no se envia el token
+  test("Debe devolver un estado 401 si no se envia el token", async () => {
+    // datos para obtener un producto
+    const data = {
+      id: 10,
+    };
 
-  //   //testear que no se pueda crear un producto sin alguno de los datos requeridos
-  //   test("Debe responder con un código de estado 400 y la información de los datos requeridos", async () => {
-  //     const data = {};
-  //     const { statusCode } = await request(app)
-  //       .post("/api/v3/products")
-  //       .send(data);
-  //     expect(statusCode).toBe(400);
-  //     expect(body).toEqual(
-  //       expect.objectContaining({
-  //         errors: expect.any(Array),
-  //       })
-  //     );
-  //   });
+    const product = await models.products.findByPk(data.id);
+
+    //envio de los datos
+    const { statusCode, body } = await request(app)
+      .get(`/api/v3/products/${data.id}`)
+      .send(data);
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(401);
+
+    // comprobar respuesta
+    expect(body).toEqual(
+      expect.objectContaining({
+        msg: "Token Invalido",
+        ok: false,
+      })
+    );
+  });
+
+  //Test debe devolver un estado 404 si el producto no existe
+  test("Debe devolver un estado 404 si el producto no existe", async () => {
+    // datos para obtener un producto
+    const data = {
+      id: 600,
+    };
+
+    // creacion del token
+    const god_user = {
+      id: 1,
+      username: "siacobo0",
+    };
+    const token = await generateJWT(god_user);
+
+    //envio de los datos
+    const { statusCode, body } = await request(app)
+      .get(`/api/v3/products/${data.id}`)
+      .auth(token, { type: "bearer" })
+      .send(data);
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(404);
+
+    // comprobar respuesta
+    expect(body).toEqual(
+      expect.objectContaining({
+        msg: "El producto no existe",
+      })
+    );
+  });
 });
--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,9 +2,13 @@ const request = require("supertest");
 const { app, server } = require("../server");
 const { generateJWT } = require("../helpers/generateJWT");
 
+afterEach(() => {
+  server.close();
+});
+
 //test de ruta get users/:id
 describe("GET /api/v3/users/:id", () => {
-  test("Usuario encontrado", async () => {
+  test("Debe devolver un estado 200 - Usuario encontrado", async () => {
     // datos para obtener un usuario
     const data = {
       id: 2,
@@ -13,7 +17,7 @@ describe("GET /api/v3/users/:id", () => {
     // creacion del token
     const guest_user = {
       id: 2,
-      username: "caralos",
+      username: "carlos",
     };
     const token = await generateJWT(guest_user);
 
@@ -25,7 +29,6 @@ describe("GET /api/v3/users/:id", () => {
 
     // comprobar codigo de status
     expect(statusCode).toBe(200);
-    console.log(body);
 
     // comprobar respuesta
     expect(body).toEqual(
@@ -36,8 +39,8 @@ describe("GET /api/v3/users/:id", () => {
   });
 
   // test de ruta get users/:id error 404 usuario no encontrado
-  test("Usuario no encontrado", async () => {
-    // datos para crear un producto
+  test("Debe devolver un estado 404 - Usuario no encontrado", async () => {
+    // datos para obtener un usuario
     const data = {
       id: 22,
     };
@@ -57,12 +60,65 @@ describe("GET /api/v3/users/:id", () => {
 
     // comprobar codigo de status
     expect(statusCode).toBe(404);
-    console.log(body);
 
     // comprobar respuesta
     expect(body).toEqual(
       expect.objectContaining({
         message: "Usuario no encontrado",
+      })
+    );
+  });
+  // test de ruta get users/:id error 401 no se envia el token
+  test("Debe devolver un estado 401 - No se envia el token", async () => {
+    // datos para obtener un usuario
+    const data = {
+      id: 2,
+    };
+
+    // envio de los datos
+    const { statusCode, body } = await request(app).get(
+      `/api/v3/users/${data.id}`
+    );
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(401);
+
+    // comprobar respuesta
+    expect(body).toEqual(
+      expect.objectContaining({
+        msg: "Token Invalido",
+        ok: false,
+      })
+    );
+  });
+  //Debe devolver un estado 403 si el usuario no esta autorizado
+  test("Debe devolver un estado 403 ", async () => {
+    // datos para obtener un usuario
+    const data = {
+      id: 10,
+    };
+
+    // creacion del token
+    const guest_user = {
+      id: 2,
+      username: "carlos",
+    };
+    const token = await generateJWT(guest_user);
+
+    // envio de los datos
+    const { statusCode, body } = await request(app)
+      .get(`/api/v3/users/${data.id}`)
+      .auth(token, { type: "bearer" })
+      .send(data);
+
+    // comprobar codigo de status
+    expect(statusCode).toBe(403);
+
+    // comprobar respuesta
+    expect(body).toEqual(
+      expect.objectContaining({
+        msg: "Unauthorized ID does not match with logged ID (Guest)",
+        ok: false,
       })
     );
   });
